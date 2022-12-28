17ff363df9512f7e5ea8b245f99b00c7d27b950a
Helen Cris <helencrisfernandes@hotmail.com>
2022-12-28 20:32:45 -0300

    finished the information page style

---
+++ src/view/screens/Information/styles.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  text: {
+    paddingTop: 10,
+    fontSize: 16,
+  },
+  separator: {
+    padding: 20,   
+  },
+});
