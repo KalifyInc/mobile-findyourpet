191bce3d2c9b6a4937d8a803c9edbb26a33ec967
Helen Cris <helencrisfernandes@hotmail.com>
2022-12-28 20:27:38 -0300

    finished the information page

--- src/view/screens/Information/index.tsx
+++ src/view/screens/Information/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Image } from "react-native";
-import { Container, Title, TextVersion, Text } from './styles';
+// import { Container, Title, TextVersion, styles } from './styles';
+import { Text, View } from '../../components/Themed';
+
 import { RootTabScreenProps } from '../../../../types';
+import { styles } from './styles';
 
 export default function InformationScreen ({ navigation }: RootTabScreenProps<'Information'>) {
   var currentYear = new Date().getFullYear();
 
   return(
-    <Container>
-      <Title>FindYourPet</Title>
-      <TextVersion>Versão 1.0.0</TextVersion>
-      {/* <Image source={require('../../assets/images/pet_icon_green.png')}></Image> */}
-      <Text>2022 - {currentYear} Kalify Inc.</Text>
-    </Container>
+    <View style={styles.container}>
+      <Text style={styles.title}>FindYourPet</Text>
+      <Text style={styles.text}>Versão 1.0.0</Text>
+      <View style={styles.separator}>
+        <Image source={require('../../../../assets/images/pet_icon_green.png')}></Image>
+      </View>
+      <Text style={styles.text}>2022 - {currentYear} Kalify Inc.</Text>
+    </View>
   )
   
 }
