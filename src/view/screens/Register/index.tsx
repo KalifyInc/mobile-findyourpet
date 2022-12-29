8678f021a78eaea18122c59a6f8b6a0b0c20257f
Helen Cris <helencrisfernandes@hotmail.com>
2022-12-29 11:39:45 -0300

    register screen changes

--- src/view/screens/Register/index.tsx
+++ src/view/screens/Register/index.tsx
@@ -1,31 +1,85 @@
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
+import { styles } from './styles';
 import { Text, View } from '../../components/Themed';
 
+import userSignInViewModel from '../../../viewModel/userRegisterViewModel';
+
 export default function RegisterScreen() {
+  const { 
+    petPhoto,
+    setPetPhoto,
+    name,
+    setName,
+    condition,
+    setCondition,
+    localization,
+    setLocalization,
+    description,
+    setDescription,
+    telephoneNumber,
+    setTelephoneNumber,
+    handleSignIn
+  } = userSignInViewModel();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>RegisterScreen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/RegisterScreen.tsx" />
-    </View>
-  );
+      <KeyboardAvoidingView>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>CADASTRAR PETS</Text>
+        </View>      
+
+        <View style={styles.container}>
+          <Text style={styles.text}>FOTO DO ANIMAL</Text>
+          <TextInput 
+            value={petPhoto}
+            onChangeText={text=> setPetPhoto(text)}
+            style={styles.textInput}
+          ></TextInput>
+
+          <Text style={styles.text}>NOME DO ANIMAL</Text>
+          <TextInput 
+            value={name}
+            onChangeText={text=> setName(text)}
+            maxLength={10}
+            style={styles.textInput}
+          ></TextInput>
+
+          <Text style={styles.text}>QUAL É A SITUAÇÃO?</Text>
+          <TextInput 
+            value={condition}
+            onChangeText={text=> setCondition(text)}
+            style={styles.textInput}
+          ></TextInput>
+          
+          <Text style={styles.text}>BAIRRO/CIDADE DO ANIMAL</Text>
+          <TextInput 
+            value={localization}
+            onChangeText={text=> setLocalization(text)}
+            style={styles.textInput}
+            ></TextInput>
+
+          <Text style={styles.text}>DESCRIÇÃO</Text> 
+            <TextInput 
+            value={description}
+            onChangeText={text=> setDescription(text)}
+            maxLength={40}
+            multiline={true}
+            style={styles.textInput}
+            ></TextInput>
+          
+          <Text style={styles.text}>NÚMERO DE TELEFONE</Text>
+            <TextInput 
+            value={telephoneNumber}
+            onChangeText={text=> setTelephoneNumber(text)}
+            style={styles.textInput}
+            ></TextInput>
+        </View>
+        <View style={styles.containerTitle}>
+          <TouchableOpacity onPress={handleSignIn}>
+            <Text>ENVIAR</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ src/view/screens/Register/styles.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'flex-start',
+    padding: 40
+  },
+  containerTitle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  text: {
+    paddingTop: 20,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    paddingTop: 8,
+    marginTop: 10,
+    borderWidth: 1,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 10,
+  }
+});
